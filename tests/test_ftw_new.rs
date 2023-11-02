e111ea10fc956b8b3a31de92cc9994e2008953a8
Michael Angelo Calimlim <macalimlim@gmail.com>
2023-11-02 13:07:25 +0800

    add more tests for template tag

--- tests/test_ftw_new.rs
+++ tests/test_ftw_new.rs
@@ -32,6 +32,7 @@ fn test_ftw_new() {
     assert!(project.read(".gitignore").contains("bin/*"));
     assert!(project.read(".gitignore").contains("export_presets.cfg"));
     assert!(project.read(".gitignore").contains("lib/*"));
+    assert!(project.read(".tag").contains("v1.4.0"));
     assert!(project
         .read("rust/Cargo.toml")
         .contains(&project.get_name()));
@@ -45,6 +46,72 @@ fn test_ftw_new_with_latest_tag() {
         .arg(&project.get_name())
         .arg("default")
         .arg("latest")
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("SUCCESS").from_utf8());
+    assert!(project.exists(".gitignore"));
+    assert!(project.exists("Cargo.toml"));
+    assert!(project.exists("Makefile"));
+    assert!(project.exists("godot/default_env.tres"));
+    assert!(project.exists("godot/export_presets.cfg"));
+    assert!(project.exists("godot/native/game.gdnlib"));
+    assert!(project.exists("godot/project.godot"));
+    assert!(project.exists("rust/Cargo.toml"));
+    assert!(project.exists("rust/src/lib.rs"));
+    assert!(!project.exists("LICENSE"));
+    assert!(!project.exists(".travis.yml"));
+    assert!(!project.exists("sh"));
+    assert!(project.read(".gitignore").contains(".ftw"));
+    assert!(project.read(".gitignore").contains("bin/*"));
+    assert!(project.read(".gitignore").contains("export_presets.cfg"));
+    assert!(project.read(".gitignore").contains("lib/*"));
+    assert!(project.read(".tag").contains("v1.4.0"));
+    assert!(project
+        .read("rust/Cargo.toml")
+        .contains(&project.get_name()));
+}
+
+#[test]
+fn test_ftw_new_with_v140_tag() {
+    let project = Project::new();
+    ftw()
+        .arg("new")
+        .arg(&project.get_name())
+        .arg("default")
+        .arg("v1.4.0")
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("SUCCESS").from_utf8());
+    assert!(project.exists(".gitignore"));
+    assert!(project.exists("Cargo.toml"));
+    assert!(project.exists("Makefile"));
+    assert!(project.exists("godot/default_env.tres"));
+    assert!(project.exists("godot/export_presets.cfg"));
+    assert!(project.exists("godot/native/game.gdnlib"));
+    assert!(project.exists("godot/project.godot"));
+    assert!(project.exists("rust/Cargo.toml"));
+    assert!(project.exists("rust/src/lib.rs"));
+    assert!(!project.exists("LICENSE"));
+    assert!(!project.exists(".travis.yml"));
+    assert!(!project.exists("sh"));
+    assert!(project.read(".gitignore").contains(".ftw"));
+    assert!(project.read(".gitignore").contains("bin/*"));
+    assert!(project.read(".gitignore").contains("export_presets.cfg"));
+    assert!(project.read(".gitignore").contains("lib/*"));
+    assert!(project.read(".tag").contains("v1.4.0"));
+    assert!(project
+        .read("rust/Cargo.toml")
+        .contains(&project.get_name()));
+}
+
+#[test]
+fn test_ftw_new_with_v130_tag() {
+    let project = Project::new();
+    ftw()
+        .arg("new")
+        .arg(&project.get_name())
+        .arg("default")
+        .arg("v1.3.0")
         .assert()
         .success()
         .stdout(predicates::str::contains("SUCCESS").from_utf8());
